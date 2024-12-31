33ec360e950a78fccbf9e6de365c55f96a9c247d
danielhabib3 <danielhabib301@gmail.com>
2024-12-31 20:50:45 +0200

    The api calls between backend and frontend working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Paroisse_Lyon_Front';
+
+  constructor(private http: HttpClient) {}
+  // Get on the server
+  get() {
+    this.http.get('http://localhost:8080/api/hello', { responseType: 'text' }).subscribe((res: string) => {
+      // convert the response to a string
+      console.log(res);
+    });
+  }
 }
